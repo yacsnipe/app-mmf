12d2f5539810da971ee2c7178c3be83d9ff42ff8
Yacine El Youssoufi <yacinee@mmforest.be>
2023-02-28 13:56:48 +0100

    fix deployment

---
+++ app-mmf/src/agenda/agenda.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-agenda',
+  templateUrl: './agenda.component.html',
+  styleUrls: ['./agenda.component.css']
+})
+export class AgendaComponent {
+
+}
---
+++ app-mmf/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'agenda',
+    pathMatch: 'full'
+  },
+  { path: 'agenda', loadChildren: () => import('../agenda/agenda.module').then(m => m.AgendaModule) }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
+
